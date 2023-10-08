51bf1ca8d6b466db90c1fcf6523fbf5afc6c38d0
LiviaMedeiros <livia@cirno.name>
2023-10-08 21:34:33 +0800

    use statements closer to internal
    this is downgrade, but it's aligned with internal logic

--- madomagi.mjs
+++ madomagi.mjs
@@ -12,17 +12,16 @@ export default class Madomagi extends Map {
 
   constructor($) {
     $ = new Database($, { create: true });
-    $.exec('pragma journal_mode = WAL;');
     $.query('create table if not exists asset_json(file char(128) primary key, etag char(128));').run();
     $.query('create table if not exists download_asset(path char(128) primary key, md5 char(128));').run();
     $.query('create table if not exists delete_asset(path char(128) primary key);').run();
     super($.query('select path, md5 from download_asset;').values());
     this.#db = $;
     this.#getEtag = $.query('select etag from asset_json where file = $file;');
-    this.#updateEtag = $.query('insert into asset_json values($file, $etag) on conflict(file) do update set etag = $etag;');
-    this.#updater = $.prepare('insert into download_asset values($path, $md5) on conflict(path) do update set md5 = $md5;');
+    this.#updateEtag = $.query('insert or replace into asset_json(file, etag) values($file, $etag)');
+    this.#updater = $.prepare('insert or replace into download_asset(path, md5) values($path, $md5);');
     this.#update = $.transaction($ => $.map($ => this.#updater.run($)));
-    this.#deleter = $.prepare('delete from download_asset where path = $path and md5 = $md5;');
+    this.#deleter = $.prepare('delete from download_asset where path = $path;');
     this.#delete = $.transaction($ => $.map($ => this.#deleter.run($)));
   }
 
@@ -48,9 +47,9 @@ export default class Madomagi extends Map {
     this.#getEtag.finalize();
     this.#updateEtag.finalize();
     this.#updater.finalize();
-    await Promise.all([...this.entries()].map(($md5, $path) =>
-      rm(new URL($path, import.meta.url), { force: true }).then(() => ({ $path, $md5 }))
-    )).then($ => this.#delete($));
+    await Promise.all([...this.keys()].map(async $path => (
+      await rm(new URL($path, import.meta.url), { force: true }), { $path }
+    ))).then($ => this.#delete($));
     this.#deleter.finalize();
     this.#db.close();
   }
